6061b8e28b61bd28eb0e1802e1a3d1f7b97b021c
EmrullahD <emrullahdurumlu@hotmail.com>
2021-08-30 23:18:00 +0300

    Added ColorsController & updated ColorDao

---
+++ backend/rent-a-car/src/main/java/com/durumluemrullah/rentacar/controllers/ColorsController.java
@@ -0,0 +1,43 @@
+package com.durumluemrullah.rentacar.controllers;
+
+import com.durumluemrullah.rentacar.dataAccess.ColorDao;
+import com.durumluemrullah.rentacar.entities.Color;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+
+@RestController
+@RequestMapping(path = "/colors")
+public class ColorsController {
+
+    @Autowired
+    private ColorDao colorDao;
+
+    @GetMapping(path = "/getAll")
+    public ResponseEntity<List<Color>> getAll(){
+        return new ResponseEntity<>(colorDao.getAll(), HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/create")
+    public ResponseEntity<String> create(@RequestBody Color color){
+        colorDao.create(color);
+        return new ResponseEntity<>("Olusturuldu",HttpStatus.CREATED);
+    }
+
+    @PostMapping(path = "/update")
+    public ResponseEntity<String> update(@RequestBody Color color){
+        colorDao.update(color);
+        return new ResponseEntity<>("Güncellendi",HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/delete/{id}")
+    public ResponseEntity<String> delete(@PathVariable(name = "id") int id){
+        colorDao.delete(id);
+        return new ResponseEntity<>("Silindi",HttpStatus.OK);
+    }
+
+}
--- backend/rent-a-car/src/main/java/com/durumluemrullah/rentacar/dataAccess/ColorDao.java
+++ backend/rent-a-car/src/main/java/com/durumluemrullah/rentacar/dataAccess/ColorDao.java
@@ -1,12 +1,56 @@
 package com.durumluemrullah.rentacar.dataAccess;
 
+import com.durumluemrullah.rentacar.entities.Color;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 @Repository
 public class ColorDao {
 
-    private final String SELECT_QUERY="";
-    private final String INSERT_QUERY="";
-    private final String DELETE_QUERY="";
-    private final String UPDATE_QUERY="";
+    private final String INSERT_QUERY="INSERT INTO public.\"COLORS\"(name) VALUES (:name);";
+    private final String SELECT_QUERY="SELECT * FROM public.\"COLORS\"";
+    private final String DELETE_QUERY="DELETE FROM public.\"COLORS\" WHERE color_id=:id;";
+    private final String UPDATE_QUERY="UPDATE public.\"COLORS\" SET name=:name WHERE color_id=:id;";
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public void create(Color color){
+        HashMap<String, Object> mapParams= new HashMap<>();
+        mapParams.put("name",color.getName());
+
+        namedParameterJdbcTemplate.update(INSERT_QUERY,mapParams);
+    }
+
+    public List<Color> getAll(){
+        List<Color> colors = namedParameterJdbcTemplate.query(SELECT_QUERY, new RowMapper<Color>() {
+            @Override
+            public Color mapRow(ResultSet resultSet, int i) throws SQLException {
+                return new Color(resultSet.getInt("color_id"),resultSet.getString("name"));
+            }
+        });
+        return colors;
+    }
+
+    public void delete(int id){
+        HashMap<String, Object> mapParams= new HashMap<>();
+        mapParams.put("id",id);
+
+        namedParameterJdbcTemplate.update(DELETE_QUERY,mapParams);
+    }
+
+    public void update(Color color){
+        HashMap<String, Object> mapParams= new HashMap<>();
+        mapParams.put("id",color.getColorId());
+        mapParams.put("name",color.getName());
+
+        namedParameterJdbcTemplate.update(UPDATE_QUERY,mapParams);
+    }
+
 }
